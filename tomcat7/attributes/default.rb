758b59246520b3cda4cdea7dfe08e9ed08fb5de9
Vlad M <solarvm@gmail.com>
2012-01-02 06:09:40 +0000

    tested // workable

--- tomcat7/attributes/default.rb
+++ tomcat7/attributes/default.rb
@@ -8,14 +8,16 @@ tc7group = "tomcat"
 default["tomcat"]["port"] = 8080
 default["tomcat"]["ssl_port"] = 8443
 default["tomcat"]["ajp_port"] = 8009
-node[:tomcat7][:java_options] = " -Xmx128M -Dajva.awt.headless=true"
-node[:tomcat7][:use_security_manager] = "no"
+set[:tomcat7][:java_options] = " -Xmx128M -Dajva.awt.headless=true"
+set[:tomcat7][:use_security_manager] = "no"
 ##
 #set ["tomcat"]["home"] = ""
 #set ["tomcat"]["base"] = ""
 #set ["tomcat"]["config_dir"] = ""
 #set ["tomcat"]["log_dir"] = ""
-node[:tomcat7][:home] = "#{tc7target}/tomcat"
-node[:tomcat7][:base] = "#{tc7target}/tomcat"
-node[:tomcat7][:config_dir] = "#{tc7target}/tomcat/conf"
-node[:tomcat7][:log_dir] = "#{tc7target}/tomcat/logs"
+set[:tomcat7][:home] = "#{tc7target}/tomcat"
+set[:tomcat7][:base] = "#{tc7target}/tomcat"
+set[:tomcat7][:config_dir] = "#{tc7target}/tomcat/conf"
+set[:tomcat7][:log_dir] = "#{tc7target}/tomcat/logs"
+set[:tomcat7][:user] = "#{tc7user}"
+set[:tomcat7][:group] = "#{tc7group}"
